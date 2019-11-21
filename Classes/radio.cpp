fc89eea5f71816204b31bba94bd278aaa498f4e3
Gilberto Echeverria <g.echeverria@tec.mx>
2019-11-21 13:50:38 -0600

    Completed radio class

--- Classes/radio.cpp
+++ Classes/radio.cpp
@@ -38,12 +38,42 @@ Radio::Radio(int _volume, float _frequency)
 
 void Radio::tuneUp()
 {
-    frequency += 0.5;
+    if (frequency >= 105)
+    {
+        frequency = 90;
+    }
+    else
+    {
+        frequency += 0.5;
+    }
 }
 
 void Radio::tuneDown()
 {
-    frequency -= 0.5;
+    if (frequency <= 90)
+    {
+        frequency = 105;
+    }
+    else
+    {
+        frequency -= 0.5;
+    }
+}
+
+void Radio::volumeUp()
+{
+    if (volume < 10)
+    {
+        volume++;
+    }
+}
+
+void Radio::volumeDown()
+{
+    if (volume > 0)
+    {
+        volume--;
+    }
 }
 
 int main()
@@ -51,12 +81,36 @@ int main()
     // Create an instance
     Radio my_radio;
     Radio another_radio(0, 100.5);
+    int option;
 
-    cout << "My radio.frequency: " << my_radio.getFrequency() << endl;
-    cout << "Another.frequency: " << another_radio.getFrequency() << endl;
-
-    another_radio.tuneUp();
-    cout << "Another.frequency: " << another_radio.getFrequency() << endl;
+    while (true)
+    {
+        cout << "Current frequency: " << another_radio.getFrequency() << endl;
+        cout << "Current volume: " << another_radio.getVolume() << endl;
+        cout << "RADIO OPTIONS:" << endl;
+        cout << "1. Tune Up" << endl;
+        cout << "2. Tune Down" << endl;
+        cout << "3. Volume Up" << endl;
+        cout << "4. Volume Down" << endl;
+        cout << "Select option: ";
+        cin >> option;
+        if (option == 1)
+        {
+            another_radio.tuneUp();
+        }
+        else if (option == 2)
+        {
+            another_radio.tuneDown();
+        }
+        if (option == 3)
+        {
+            another_radio.volumeUp();
+        }
+        else if (option == 4)
+        {
+            another_radio.volumeDown();
+        }
+    }
 
     return 0;
 }
