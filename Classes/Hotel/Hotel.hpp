8249628974eb4f73c1a29d1cdff09869ebc46063
Gilberto Echeverria <g.echeverria@tec.mx>
2019-11-26 15:05:05 -0600

    Completed example for the Hotel class

--- Classes/Hotel/Hotel.hpp
+++ Classes/Hotel/Hotel.hpp
@@ -27,3 +27,79 @@ class Hotel {
         void display();
 };
 
+Hotel::Hotel(string _name, int _rooms)
+{
+    name = _name;
+    num_rooms = _rooms;
+    // Create the list of all the rooms needed
+    rooms.resize(num_rooms);
+    // Assign the room number to each
+    for (int i=0; i<num_rooms; i++)
+    {
+        rooms[i].setNumber(i + 1);
+    }
+}
+
+int Hotel::nextAvailable()
+{
+    for (int i=0; i<num_rooms; i++)
+    {
+        if (rooms[i].isAvailable())
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool Hotel::hasFreeRooms()
+{
+    if (nextAvailable() >= 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool Hotel::checkIn(Person _guest)
+{
+    int nextRoom = nextAvailable();
+
+    if (nextRoom >= 0)
+    {
+        rooms[nextRoom].registerGuest(_guest);
+        cout << "Guest: " << _guest.getName() << " assigned to room " << nextRoom << endl;
+        return true;
+    }
+    else
+    {
+        cout << "No rooms avaialble. Sorry" << endl;
+        return false;
+    }
+}
+
+bool Hotel::checkOut(Person _guest)
+{
+    // Look for the guest in all the rooms
+    for (int i=0; i<num_rooms; i++)
+    {
+        if (rooms[i].getGuestName() == _guest.getName())
+        {
+            rooms[i].free();
+            return true;
+        }
+    }
+    return false;
+}
+
+void Hotel::display()
+{
+    cout << "\n== HOTEL " << name << " ==" << endl;
+    for (int i=0; i<num_rooms; i++)
+    {
+        rooms[i].display();
+    }
+}
